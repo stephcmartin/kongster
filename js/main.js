f943811576faf0bf07eec6b6e9e3be4a7de59afa
stephcmartin <martin.stephaniechow@gmail.com>
2017-10-22 13:27:43 -0400

    Removed Excess Blocks, Added Cursor Functions
    - Removed extra blocks, characters etc and started building game's blocks
    - Added Cursor Functions to move character left and right

--- js/main.js
+++ js/main.js
@@ -10,54 +10,73 @@ var GameState = {
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
 
-    //start physics engine
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.game.physics.arcade.gravity.y = 1000
+    //start physics engine on a global level
+    //(using phaser global using individual sprite)
+    this.game.physics.startSystem(Phaser.Physics.ARCADE); // this starts the Arcade System.
+    // Arcade systems stimulates everything in rectangles
+    this.game.physics.arcade.gravity.y = 1000 // set the gravity for the whole game
+    // the value changes the speed
+
+    this.cursors = this.game.input.keyboard.createCursorKeys() // allows users to use the keyboard to play the game
+    this.RUNNING_SPEED = 180
+    this.JUMPING_SPEED = 550
   },
 
   //load the game assets before the game starts
   preload: function() {
-this.load.image('ground', 'assets/images/ground.png')
-this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1)
+    this.load.image('ground', 'assets/images/ground.png')
+    this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1)
+    this.load.image('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1)
+    this.load.image('goal', 'assets/images/gorilla3.png')
+    this.load.image('arrowButton', 'assets/images/arrowButton.png')
+    this.load.image('actionButton', 'assets/images/actionButton.png')
+    this.load.image('barrel', 'assets/images/barrel.png')
+    this.load.image('platform', 'assets/images/platform.png')
 
   },
   //executed after everything is loaded
   create: function() {
-    this.ground = this.add.sprite(100, 200, 'ground') //
-    this.ground.anchor.setTo(0.5)
-    this.ground.x = 300
-    // this.ground.scale.setTo(1,2) // Changes to ratio
-    // this.ground.angle = 60 // this changes the angle of sprite
+    // ground
+    this.ground = this.add.sprite(0, 530, 'ground')
     this.game.physics.arcade.enable(this.ground)
-    // this.ground.body.allowGravity = false
+    this.ground.body.allowGravity = false // the body gives us access to prototypes e.g velocity and math
+    this.ground.body.immovable = true // this means you can't move it even if you step on it
 
-    this.ground2 = this.add.sprite(100, 400, 'ground') //
-    this.ground2.anchor.setTo(0.5)
-    this.ground2.x = 300
-    this.game.physics.arcade.enable(this.ground2)
-    this.ground2.body.allowGravity = false // this makes it NOT fall. Uncommit to fall.
-    this.ground2.body.immovable = true // this makes the ground NOT move.
-
-    this.ground3 = this.add.sprite(100, 530, 'ground')
-
-    // group of playforms
-    this.platforms = this.add.group() //place holder for spirte that can be used as if it is a sprite
-    this.platforms.enableBody = true
-    this.platforms.create(50, 150, 'player')
-    this.platforms.create(150, 50, 'player')
-    this.platforms.setAll('body.immovable', true)
-    this.platforms.setAll('body.allowGravity', true) // true or false sets or removes gravity to the set of images.
+    // platform
+    this.platform = this.add.sprite(0, 300, 'platform')
+    this.game.physics.arcade.enable(this.platform)
+    this.platform.body.allowGravity = false
+    this.platform.body.immovable = true
 
     // player
-    this.player = this.add.sprite(100, 0, 'player', 3)
+    this.player = this.add.sprite(100, 200, 'player', 3)
+    this.player.anchor.setTo(0.5)
     this.player.animations.add('walking', [0,1,2,1], 6, true) // this is the animation for the walking player.
     // the array shows the order of the images
-    this.player.play('walking') // we call play the order loop options.
+    this.game.physics.arcade.enable(this.player) // we call play the order loop options.
+    // this.player.play('walking') // we call play the order loop options.
+
+    // // group of playforms
+    // this.platforms = this.add.group() //place holder for spirte that can be used as if it is a sprite
+    // this.platforms.setAll('body.immovable', true)
+    // this.platforms.setAll('body.allowGravity', true) // true or false sets or removes gravity to the set of images.
+
   },
 
+// update method gets called multiple times a second
   update: function() {
-    // this.ground.angle += 1 // anything that you want to be doing at all times
-    this.game.physics.arcade.collide(this.ground, this.ground2) // specify which two groups are going to collide
+    // this.ground.angle += 1 // this spins the ground
+    this.game.physics.arcade.collide(this.ground, this.player) // specify which two groups are going to collide
+    this.game.physics.arcade.collide(this.platform, this.player) // These two things do not interfere with each other
+
+    this.player.body.velocity.x = 0
+
+    if(this.cursors.left.isDown){
+      this.player.body.velocity.x = -this.RUNNING_SPEED
+    }
+    if(this.cursors.right.isDown){
+      this.player.body.velocity.x = this.RUNNING_SPEED
+    }
   },
 
 };
