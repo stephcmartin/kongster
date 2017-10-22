79f974f02a5a3d7999c41ed614b99b45a589fb6b
stephcmartin <martin.stephaniechow@gmail.com>
2017-10-22 14:50:19 -0400

    Created External Text file with Data
    - loaded into main.js and parsed it as a JSON object

--- js/main.js
+++ js/main.js
@@ -38,6 +38,8 @@ var GameState = {
     this.load.image('barrel', 'assets/images/barrel.png')
     this.load.image('platform', 'assets/images/platform.png')
     this.load.image('spacebar', 'assets/images/spacebar.png')
+    // pre-load jason text file
+    this.load.text('level', 'assets/data/level.json')
 
   },
   //executed after everything is loaded
@@ -55,7 +57,7 @@ var GameState = {
     // this.platform.body.immovable = true
 
     // player
-    this.player = this.add.sprite(10, 545, 'player', 3)
+    this.player = this.add.sprite(0, 545, 'player', 3)
     this.player.anchor.setTo(0.5)
     this.player.animations.add('walking', [0,1,2,1], 6, true) // this is the animation for the walking player.
     // the array shows the order of the images
@@ -69,16 +71,19 @@ var GameState = {
 
     // group of platform array
     // x and y locations for platform
-    let platformData = [
-        {"x": 0, "y": 430},
-        {"x": 45, "y": 560},
-        {"x": 90, "y": 290},
-        {"x": 0, "y": 140}
-      ]
+    // let platformData = [
+    //     {"x": 0, "y": 430},
+    //     {"x": 45, "y": 560},
+    //     {"x": 90, "y": 290},
+    //     {"x": 0, "y": 140}
+    //   ]
+
+//parse the file
+this.levelData = JSON.parse(this.game.cache.getText('level'))
 
       this.platforms = this.add.group()
       this.platforms.enableBody = true
-      platformData.forEach(function(element){
+      this.levelData.platformData.forEach(function(element){
         this.platforms.create(element.x, element.y, 'platform')
       }, this)
       this.platforms.setAll('body.immovable', true) // this means the platforms will not move
